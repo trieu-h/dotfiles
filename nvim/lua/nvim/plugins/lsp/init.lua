244dd52aaa61ff77420e940774fa6e51338dc8ca
Trieu Le Huynh Hai <trieu.lehuynhhai@finantix.com>
2021-05-22 14:55:18 +0700

    add ocaml language server

--- nvim/lua/nvim/plugins/lsp/init.lua
+++ nvim/lua/nvim/plugins/lsp/init.lua
@@ -52,7 +52,7 @@ end
 --
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "tsserver", "angularls", "cssls", "html"}
+local servers = { "tsserver", "angularls", "cssls", "html", "ocamllsp"}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
