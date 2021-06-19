2208c2420d4d0c864d66f8a792396b62cb20993f
Trieu <trieu.lehuynhhai@finantix.com>
2021-06-19 17:20:29 +0700

    Add bash script to create symlinks

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+CONFIG_DIR="$HOME/.config"
+
+echo -e "Create symlinks...."
+
+# Delete existing dot files and folders
+sudo rm -rf $HOME/.gitconfig
+sudo rm -rf $HOME/.zshrc
+
+# Create symlinks in the home folder
+ln -sf $CONFIG_DIR/.gitconfig $HOME/.gitconfig
+ln -sf $CONFIG_DIR/.zshrc $HOME/.zshrc
+
+echo -e "Finished creating"
