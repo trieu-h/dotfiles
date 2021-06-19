fddc1139657ad09701228c38dc344b7667b4cb91
Hai Trieu <lehuynhhaitrieu1996@gmail.com>
2021-06-19 16:30:28 +0700

    commit setting

--- nvim/lua/nvim/plugins/filetree/init.lua
+++ nvim/lua/nvim/plugins/filetree/init.lua
@@ -4,7 +4,7 @@ local g = vim.g
 vim.o.termguicolors = true
 
 g.nvim_tree_side = "left"
-g.nvim_tree_width = 50
+g.nvim_tree_width = 30
 g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 g.nvim_tree_auto_open = 0
 g.nvim_tree_auto_close = 0
