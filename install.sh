1ffcf12b719b11a6ee0c4c491780b680230f8b19
Trieu <trieu.lehuynhhai@finantix.com>
2021-07-10 19:52:55 +0700

    Modify install script

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ echo -e "Creating symlinks...."
 # Delete existing dot files and folders
 sudo rm -rf $HOME/.gitconfig
 sudo rm -rf $HOME/.zshrc
+sudo rm -rf $HOME/.tmux.conf
 
 # Create symlinks in the home folder
 ln -sf $CONFIG_DIR/.gitconfig $HOME/.gitconfig
 ln -sf $CONFIG_DIR/.zshrc $HOME/.zshrc
+ln -sf $CONFIG_DIR/.tmux.conf $HOME/.tmux.conf
 
 echo -e "Finished creating"
